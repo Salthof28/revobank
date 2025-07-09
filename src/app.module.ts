50fc0e9a81ada5923709391a8dc3a5df3fbfa827
Salman <salmanalthof31@gmail.com>
2025-07-09 17:41:53 +0700

    feat: create middleware for logging and rate limit

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,18 @@
-import { Module } from '@nestjs/common';
+import { MiddlewareConsumer, Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AccountsModule } from './accounts/accounts.module';
 import { UserModule } from './user/user.module';
+import { LoggerMiddleware } from './global/middlewares/logger.middleware';
+import { RateLimitMiddleware } from './global/middlewares/rate-limit.middleware';
 
 @Module({
   imports: [AccountsModule, UserModule],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer.apply(LoggerMiddleware, RateLimitMiddleware).forRoutes('*');
+  }
+}
---
+++ src/global/middlewares/rate-limit.middleware.ts
@@ -0,0 +1,42 @@
+import { BadRequestException, Injectable, NestMiddleware } from "@nestjs/common";
+import { NextFunction, Request, Response } from "express";
+
+
+interface RateLimitParam {
+    [ip: string]: {
+        countReq: number,
+        timeRequest: number
+    }
+}
+@Injectable()
+export class RateLimitMiddleware implements NestMiddleware {
+    private request: RateLimitParam = {}
+
+    private readonly windowMs = 10*1000;
+    private readonly maxRequest = 10;
+    use(req: Request, res: Response, next: NextFunction){
+        const ip = String(req.ip);
+        const currentTime = Date.now();
+
+        if(!this.request[ip]) {
+            this.request[ip] = {
+                countReq: 1,
+                timeRequest: currentTime
+            }
+        }
+        else {
+            const checkTimeReset = currentTime - this.request[ip].timeRequest;
+            if(checkTimeReset < this.windowMs) {
+                this.request[ip].countReq ++;
+                if(this.request[ip].countReq > this.maxRequest) throw new BadRequestException('to many request, wait a moment')
+            }
+            else{
+                this.request[ip] = {
+                    countReq: 1,
+                    timeRequest: currentTime
+                }
+            }
+        }
+        next()
+    }
+}
