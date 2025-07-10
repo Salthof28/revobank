3df7be51f9d153e697620e5707f0b8f497c64c1e
Salman <salmanalthof31@gmail.com>
2025-07-10 16:54:46 +0700

    feat: add try catch in cotroller

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,24 +1,44 @@
-import { Body, Controller, Get, Param, ParseIntPipe, Patch, Post, Query } from '@nestjs/common';
+import { Body, Controller, Get, HttpStatus, InternalServerErrorException, Param, ParseIntPipe, Patch, Post, Query } from '@nestjs/common';
 import { UserService } from './user.service';
 import { UpdateUserDto } from './dto/req/update-user.dto';
+import { User } from './entities/user.entity';
+import { RepositoryException } from 'src/global/exception/exception.repository';
 
 @Controller('user')
 export class UserController {
     constructor(private userService: UserService) {}
 
     @Get()
-    getAllUsers(@Query('name') name: string) {
-        return this.userService.getAllUsers({name})
+    getAllUsers(@Query('name') name: string): User[] {
+        try{
+            const allUsers = this.userService.getAllUsers({name})
+            return allUsers;
+        } catch (error) {
+            if(error instanceof RepositoryException) throw error;
+            throw new InternalServerErrorException()
+        }
     }
 
     @Get('/:id')
-    getUser(@Param('id', ParseIntPipe) id: number) {
-        return this.userService.getUser({id});
+    getUser(@Param('id', ParseIntPipe) id: number): User {
+        try {
+            const user = this.userService.getUser({id});
+            return user;
+        } catch (error) {
+            if(error instanceof RepositoryException) throw error;
+            throw new InternalServerErrorException()
+        }
     }
 
     @Patch('/:id')
-    updateUser(@Param('id', ParseIntPipe) id: number, @Body() body: UpdateUserDto) {
-        return this.userService.updateUser({id, body})
+    updateUser(@Param('id', ParseIntPipe) id: number, @Body() body: UpdateUserDto): User {
+        try{
+            const updateUser = this.userService.updateUser({id, body});
+            return updateUser;
+        } catch (error) {
+            if(error instanceof RepositoryException) throw error;
+            throw new InternalServerErrorException();
+        }
     }
 
 }
